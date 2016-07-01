c3c4a2eb3579068a4becccd7e4d81babc5c7a8fb
Martin Vysny <mvy@whitestein.com>
2016-07-01 11:40:31 +0200

    minor: added language injection

--- kotlinee-example-crud/src/main/java/com/github/kotlinee/example/crud/WelcomeView.kt
+++ kotlinee-example-crud/src/main/java/com/github/kotlinee/example/crud/WelcomeView.kt
@@ -19,6 +19,7 @@ class WelcomeView: VerticalLayout(), View {
     init {
         setSizeFull(); isMargin = true
         label {
+            //language=HTML
             html("""<h3>Kotlinee</h3>Welcome to the Kotlinee demo. Kotlinee provides means to creating rich Vaadin apps:
             <ul><li>Provides Vaadin DSL builder support</li>
             <li>Provides simple database access via db {} function</li></ul>
