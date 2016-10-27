3b423f83c542dfc250b07ad8ee0dfb0056c77255
Martin Vysny <mavi@vaadin.com>
2016-10-27 22:04:35 +0300

    Fix around ShortcutListeners not serializable

--- kotlinee-framework/src/main/java/com/github/kotlinee/framework/vaadin/ShortcutListeners.java
+++ kotlinee-framework/src/main/java/com/github/kotlinee/framework/vaadin/ShortcutListeners.java
@@ -1,5 +1,6 @@
 package com.github.kotlinee.framework.vaadin;
 
+import kotlin.jvm.functions.Function0;
 import org.jetbrains.annotations.NotNull;
 
 import com.vaadin.event.ShortcutListener;
@@ -8,11 +9,11 @@ import com.vaadin.event.ShortcutListener;
  * @author mvy
  */
 public class ShortcutListeners {
-	public static ShortcutListener listener(int keyCode, @NotNull int[] modifierKeys, final @NotNull Runnable action) {
+	public static ShortcutListener listener(int keyCode, @NotNull int[] modifierKeys, final @NotNull Function0<?> action) {
 		return new ShortcutListener(null, keyCode, modifierKeys) {
 			@Override
 			public void handleAction(Object sender, Object target) {
-				action.run();
+				action.invoke();
 			}
 		};
 	}
