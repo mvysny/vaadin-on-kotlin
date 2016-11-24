8c09f1ddb3e9258935db4a34b95d907de04c16a3
Martin Vysny <mavi@vaadin.com>
2016-11-24 13:03:47 +0200

    more kotlindoc

--- kotlinee-example-crud/src/main/java/com/github/kotlinee/example/crud/LastAddedPersonCache.kt
+++ kotlinee-example-crud/src/main/java/com/github/kotlinee/example/crud/LastAddedPersonCache.kt
@@ -7,9 +7,10 @@ import org.slf4j.LoggerFactory
 import java.io.Serializable
 
 /**
- * A demo of a session-bound class which caches the last added person. Use the [lastAddedPersonCache] global property to retrieve instances.
+ * A demo of a session-bound class which caches the last added person. Use the [Session.lastAddedPersonCache] global property to retrieve instances.
  *
- * Note the private constructor, to avoid accidentally constructing this class by hand. To correctly manage instances of this class,
+ * Note the private constructor, to avoid accidentally constructing this class by hand. To correctly manage instances of this class, simply let
+ * [SessionScoped] class to do that for you.
  * @author mvy
  */
 class LastAddedPersonCache private constructor() : Serializable {
@@ -30,6 +31,6 @@ class LastAddedPersonCache private constructor() : Serializable {
 /**
  * Retrieves the session-scoped instance of this class, creating and binding it to the current session if necessary.
  *
- * WARNING: you can only read the property while holding the Vaadin UI lock!
+ * WARNING: you can only read the property while holding the Vaadin UI lock! That is, this class is not accessible from a background thread.
  */
 val Session.lastAddedPersonCache: LastAddedPersonCache by SessionScoped.get()
