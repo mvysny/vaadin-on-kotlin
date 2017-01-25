51883a5c0f1602dcd8c2475a7b75850bf9d0d0af
Martin Vysny <mavi@vaadin.com>
2017-01-25 09:24:50 +0200

    Server.kt: fixed auto-discovery of JPA entities

--- vok-example-crud/src/test/java/com/github/vok/example/crud/Server.kt
+++ vok-example-crud/src/test/java/com/github/vok/example/crud/Server.kt
@@ -1,16 +1,34 @@
 package com.github.vok.example.crud
 
+import com.github.vok.example.crud.personeditor.Person
+import com.github.vok.framework.VaadinOnKotlin
+import org.atmosphere.util.annotation.AnnotationDetector
 import org.eclipse.jetty.annotations.AnnotationConfiguration
 import org.eclipse.jetty.plus.webapp.EnvConfiguration
 import org.eclipse.jetty.plus.webapp.PlusConfiguration
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.*
+import org.hibernate.jpa.AvailableSettings
+import javax.persistence.Entity
+import javax.persistence.Persistence
 
 
 /**
  * Runs the testing Jetty-based server. Just open http://localhost:8080/ in your browser.
  */
 fun main(args: Array<String>) {
+    // detect all entities. workaround for Hibernate not able to detect entities outside of jar files
+    // https://forum.hibernate.org/viewtopic.php?f=1&t=1043948&e=0
+    val entities = mutableListOf<Class<*>>()
+    AnnotationDetector(object : AnnotationDetector.TypeReporter {
+        override fun reportTypeAnnotation(annotation: Class<out Annotation>?, className: String?) {
+            entities.add(Class.forName(className))
+        }
+
+        override fun annotations(): Array<out Class<out Annotation>> = arrayOf(Entity::class.java)
+    }).detect("com.github")
+
+    VaadinOnKotlin.entityManagerFactory = Persistence.createEntityManagerFactory("sample", mapOf(AvailableSettings.LOADED_CLASSES to entities))
     val server = Server(8080)
     val context = WebAppContext("src/main/webapp", "/")
     context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*")
@@ -18,7 +36,4 @@ fun main(args: Array<String>) {
     server.handler = context
     server.start()
     server.join()
-    // @todo note that when you click the "CRUD Demo" tab then you'll receive an exception: SEVERE:
-    // java.lang.IllegalArgumentException: Not an entity: class com.github.vok.example.crud.personeditor.Person
-    // I have asked for help at Hibernate forums, we'll see.
 }
