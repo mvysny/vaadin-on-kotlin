03d9ff5d76d7ad7e469834879d62fcdadb79d879
Martin Vysny <mavi@vaadin.com>
2017-02-24 10:51:25 +0200

    VaadinFilters now uses PropertySet instead of Kotlin reflection

--- vok-framework/src/main/java/com/github/vok/framework/vaadin/VaadinFilters.kt
+++ vok-framework/src/main/java/com/github/vok/framework/vaadin/VaadinFilters.kt
@@ -1,13 +1,13 @@
 package com.github.vok.framework.vaadin
 
-import com.vaadin.data.Binder
-import com.vaadin.data.HasValue
+import com.vaadin.data.*
 import com.vaadin.data.provider.ConfigurableFilterDataProvider
 import com.vaadin.server.Resource
 import com.vaadin.shared.ui.ValueChangeMode
 import com.vaadin.shared.ui.datefield.DateTimeResolution
 import com.vaadin.ui.*
 import com.vaadin.ui.components.grid.HeaderRow
+import kotlinx.support.jdk8.streams.toList
 import java.io.Serializable
 import java.sql.Timestamp
 import java.time.LocalDateTime
@@ -135,27 +135,10 @@ class NumberFilterPopup : CustomField<NumberInterval<Double>?>() {
 }
 
 /**
- * Returns the type of the values present in this property.
- * @return the type, auto-converted from primitive type to corresponding boxed type, never null.
- */
-@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-fun KProperty<*>.getValueClass(): Class<*> {
-    val clazz = (returnType as KClass<*>).java
-    return when(clazz) {
-        Integer.TYPE -> Integer::class.java
-        java.lang.Long.TYPE -> Long::class.java
-        java.lang.Float.TYPE -> Float::class.java
-        java.lang.Double.TYPE -> java.lang.Double::class.java
-        java.lang.Short.TYPE -> Short::class.java
-        java.lang.Byte.TYPE -> Byte::class.java
-        else -> clazz
-    }
-}
-
-/**
  * Converts this class to its non-primitive counterpart. For example, converts `int.class` to `Integer.class`.
  * @return converts class of primitive type to appropriate non-primitive class; other classes are simply returned as-is.
  */
+@Suppress("UNCHECKED_CAST")
 val <T> Class<T>.nonPrimitive: Class<T> get() = when(this) {
     Integer.TYPE -> Integer::class.java as Class<T>
     java.lang.Long.TYPE -> Long::class.java as Class<T>
@@ -181,9 +164,13 @@ val <T> Class<T>.nonPrimitive: Class<T> get() = when(this) {
  * @param T the type of beans handled by the Grid.
  * @author mvy, stolen from Teppo Kurki's FilterTable.
  */
-abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val dataProvider: ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>) {
-
+abstract class FilterFieldFactory<T: Any>(protected val itemClass: Class<T>, val dataProvider: ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>) {
+    /**
+     * Current set of filters. Every filter component adds or removes stuff from this set on value change.
+     */
     internal val filters = mutableSetOf<JPAFilter>()
+
+    protected val properties: PropertySet<T> = BeanPropertySet.get(itemClass)
 
     /**
      * Creates the filtering component. The component may not necessarily produce values of given data types - for example,
@@ -194,9 +181,10 @@ abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val da
      * @return A field that can be assigned to the given fieldType and that is
      * *         capable of filtering given type of data. May return null if filtering of given data type with given field type is unsupported.
      */
-    abstract fun <T> createField(property: KProperty<T>): HasValue<T>?
+    abstract fun <V> createField(property: PropertyDefinition<T, V?>): HasValue<V?>?
 
-    protected fun getProperty(name: String) = clazz.members.filterIsInstance<KProperty<*>>().first { it.name == name }
+    protected fun getProperty(name: String): PropertyDefinition<T, *> =
+            properties.getProperty(name).orElse(null) ?: throw IllegalArgumentException("$itemClass has no property $name; available properties: ${properties.properties.map { it.name }}")
 
     /**
      * Creates a new Container Filter based on given value.
@@ -205,7 +193,7 @@ abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val da
      * @param property the property
      * @return a filter, may be null if no filtering is needed or if the value indicates that the filtering is disabled for this column.
      */
-    protected abstract fun <T> createFilter(value: T?, filterField: HasValue<T>, property: KProperty<T>): JPAFilter?
+    protected abstract fun <V> createFilter(value: V?, filterField: HasValue<V?>, property: PropertyDefinition<T, V?>): JPAFilter?
 
     /**
      * Binds given filtering field to a container - starts filtering based on the contents of the field, and starts watching for field value changes.
@@ -213,7 +201,7 @@ abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val da
      * the field's value to a filter.
      * @param property The bean property on which the filtering will be performed, not null.
      */
-    fun <T> bind(field: HasValue<T>, property: KProperty<T>) {
+    fun <V> bind(field: HasValue<V?>, property: PropertyDefinition<T, V?>) {
         val filterFieldWatcher = FilterFieldWatcher(field, property)
         field.addValueChangeListener(filterFieldWatcher)
     }
@@ -224,7 +212,7 @@ abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val da
      * @property property The bean property on which the filtering will be performed.
      * @param V the value type
      */
-    private inner class FilterFieldWatcher<V>(private val field: HasValue<V>, private val property: KProperty<V>) : HasValue.ValueChangeListener<V> {
+    private inner class FilterFieldWatcher<V>(private val field: HasValue<V?>, private val property: PropertyDefinition<T, V?>) : HasValue.ValueChangeListener<V?> {
 
         /**
          * The current container filter, may be null if no filtering is currently needed because the
@@ -236,7 +224,7 @@ abstract class FilterFieldFactory<T: Any>(protected val clazz: KClass<T>, val da
             valueChange()
         }
 
-        override fun valueChange(event: HasValue.ValueChangeEvent<V>?) {
+        override fun valueChange(event: HasValue.ValueChangeEvent<V?>) {
             valueChange()
         }
 
@@ -393,7 +381,7 @@ class DateFilterPopup: CustomField<DateInterval?>() {
  * Supports filter fields for dates, numbers and strings.
  * @author mvy, stolen from Teppo Kurki's FilterTable.
  */
-class DefaultFilterFieldFactory<T: Any>(clazz: KClass<T>, dataProvider: ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>) : FilterFieldFactory<T>(clazz, dataProvider) {
+class DefaultFilterFieldFactory<T: Any>(clazz: Class<T>, dataProvider: ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>) : FilterFieldFactory<T>(clazz, dataProvider) {
     /**
      * If true, number filters will be shown as a popup, which allows the user to set eq, less-than and greater-than fields.
      * If false, a simple in-place editor will be shown, which only allows to enter the eq number.
@@ -401,13 +389,13 @@ class DefaultFilterFieldFactory<T: Any>(clazz: KClass<T>, dataProvider: Configur
      * Default implementation always returns true.
      * @param property the bean property
      */
-    protected fun isUsePopupForNumericProperty(property: KProperty<*>): Boolean = true
+    protected fun isUsePopupForNumericProperty(property: PropertyDefinition<T, *>): Boolean = true
 
-    override fun <T> createField(property: KProperty<T>): HasValue<T>? {
-        val type = property.getValueClass() as Class<T>
+    override fun <V> createField(property: PropertyDefinition<T, V?>): HasValue<V?>? {
+        val type = property.type.nonPrimitive
         val field: HasValue<*>
-        if (type == java.lang.Boolean.TYPE || type == java.lang.Boolean::class.java) {
-            field = createBooleanField(property as KProperty<Boolean>)
+        if (type == java.lang.Boolean::class.java) {
+            field = createBooleanField(property as PropertyDefinition<T, Boolean?>)
         } else if (type.isEnum) {
             field = createEnumField(type, property)
         } else if (type == Date::class.java || type == Timestamp::class.java
@@ -422,49 +410,50 @@ class DefaultFilterFieldFactory<T: Any>(clazz: KClass<T>, dataProvider: Configur
             (this as Component).w = fillParent; (this as? HasValueChangeMode)?.valueChangeMode = ValueChangeMode.LAZY
         }
         @Suppress("UNCHECKED_CAST")
-        return field as HasValue<T>
+        return field as HasValue<V?>
     }
 
-    protected fun getEnumFilterDisplayName(property: KProperty<*>, constant: Enum<*>): String? = null
+    protected fun getEnumFilterDisplayName(property: PropertyDefinition<T, *>, constant: Enum<*>): String? = null
 
-    protected fun getEnumFilterIcon(property: KProperty<*>, constant: Enum<*>): Resource? = null
+    protected fun getEnumFilterIcon(property: PropertyDefinition<T, *>, constant: Enum<*>): Resource? = null
 
-    private fun <T> createEnumField(type: Class<T>, property: KProperty<T>): HasValue<*> {
-        val enumSelect = ComboBox<T>()
+    private fun <V> createEnumField(type: Class<V?>, property: PropertyDefinition<T, V?>): HasValue<V?> {
+        val enumSelect = ComboBox<V?>()
         enumSelect.setItems(*type.enumConstants)
         enumSelect.itemCaptionGenerator = ItemCaptionGenerator { e -> getEnumFilterDisplayName(property, e as Enum<*>) ?: e.name }
         return enumSelect
     }
 
-    protected fun createTextField(property: KProperty<*>): HasValue<*> = TextField()
+    protected fun createTextField(property: PropertyDefinition<T, *>): HasValue<*> = TextField()
 
-    protected fun createDateField(property: KProperty<*>): DateFilterPopup = DateFilterPopup()
+    protected fun createDateField(property: PropertyDefinition<T, *>): DateFilterPopup = DateFilterPopup()
 
-    protected fun createNumericField(type: Class<*>, propertyId: Any?) = NumberFilterPopup()
+    protected fun createNumericField(type: Class<*>, property: PropertyDefinition<T, *>) = NumberFilterPopup()
 
     /**
      * Don't forget that the returned field must be tri-state - true, false, null (to disable filtering).
      */
-    protected fun createBooleanField(property: KProperty<Boolean>): HasValue<Boolean> {
-        val booleanSelect = ComboBox<Boolean>()
+    protected fun createBooleanField(property: PropertyDefinition<T, Boolean?>): HasValue<Boolean?> {
+        val booleanSelect = ComboBox<Boolean?>()
         booleanSelect.setItems(listOf(true, false))
-        booleanSelect.itemCaptionGenerator = ItemCaptionGenerator { b -> getBooleanFilterDisplayName(property, b) ?: b.toString() }
-        booleanSelect.itemIconGenerator = IconGenerator { b -> getBooleanFilterIcon(property, b) }
+        booleanSelect.itemCaptionGenerator = ItemCaptionGenerator { b -> getBooleanFilterDisplayName(property, b!!) ?: b.toString() }
+        booleanSelect.itemIconGenerator = IconGenerator { b -> getBooleanFilterIcon(property, b!!) }
         return booleanSelect
     }
 
-    protected fun getBooleanFilterIcon(property: KProperty<Boolean>, value: Boolean): Resource? = null
+    protected fun getBooleanFilterIcon(property: PropertyDefinition<T, Boolean?>, value: Boolean): Resource? = null
 
-    protected fun getBooleanFilterDisplayName(property: KProperty<Boolean>, value: Boolean): String? = null
+    protected fun getBooleanFilterDisplayName(property: PropertyDefinition<T, Boolean?>, value: Boolean): String? = null
 
-    override fun <T> createFilter(value: T?, filterField: HasValue<T>, property: KProperty<T>): JPAFilter? = when {
+    @Suppress("UNCHECKED_CAST")
+    override fun <V> createFilter(value: V?, filterField: HasValue<V?>, property: PropertyDefinition<T, V?>): JPAFilter? = when {
         value is NumberInterval<*> -> value.toFilter(property.name)
         value is DateInterval -> value.toFilter(property.name)
-        value is String && !value.isEmpty() -> generateGenericFilter<String>(filterField as HasValue<String>, property as KProperty<String>, value.trim())
+        value is String && !value.isEmpty() -> generateGenericFilter<String>(filterField as HasValue<String?>, property as PropertyDefinition<T, String?>, value.trim())
         else -> null
     }
 
-    protected fun <T: Serializable> generateGenericFilter(field: HasValue<T>, property: KProperty<T>, value: T): JPAFilter? {
+    protected fun <V: Serializable> generateGenericFilter(field: HasValue<V?>, property: PropertyDefinition<T, V?>, value: V): JPAFilter? {
         /* Special handling for ComboBox (= enum properties) */
         if (field is ComboBox) {
             return EqFilter(property.name, value)
@@ -480,17 +469,19 @@ class DefaultFilterFieldFactory<T: Any>(clazz: KClass<T>, dataProvider: Configur
  * @param grid the owner grid.
  * @param filterFieldFactory used to create the filters themselves. If null, [DefaultFilterFieldFactory] is used.
  */
+@Suppress("UNCHECKED_CAST")
 fun <T: Any> HeaderRow.generateFilterComponents(grid: Grid<T>, itemClass: KClass<T>,
-                                                filterFieldFactory: FilterFieldFactory<T> = DefaultFilterFieldFactory(itemClass, grid.dataProvider as ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>)) {
-    val propertyIds = itemClass.members.filterIsInstance<KProperty<*>>().associateBy { it.name }
-    for (propertyId in grid.columns.map { it.id }) {
-        val property = propertyIds[propertyId]
-        val field = if (property == null) null else filterFieldFactory.createField(property)
+                                                filterFieldFactory: FilterFieldFactory<T> = DefaultFilterFieldFactory(itemClass.java,
+                                                        grid.dataProvider as ConfigurableFilterDataProvider<T, JPAFilter?, JPAFilter?>)) {
+    val properties: Map<String, PropertyDefinition<T, *>> = BeanPropertySet.get(itemClass.java).properties.toList().associateBy { it.name }
+    for (propertyId in grid.columns.map { it.id }.filterNotNull()) {
+        val property = properties[propertyId]
+        val field: HasValue<*>? = if (property == null) null else filterFieldFactory.createField(property)
         val cell = getCell(propertyId)
         if (field == null) {
             cell.text = null  // this also removes the cell from the row
         } else {
-            filterFieldFactory.bind(field, property!!)
+            filterFieldFactory.bind(field as HasValue<Any?>, property!! as PropertyDefinition<T, Any?>)
             cell.component = field as Component
         }
     }
