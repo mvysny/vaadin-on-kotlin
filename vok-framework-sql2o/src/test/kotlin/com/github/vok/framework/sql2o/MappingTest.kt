0b6d2afc31f479c2371a342867950396e23ae5a7
Martin Vysny <mavi@vaadin.com>
2017-07-08 20:01:13 +0300

    More column types added

--- vok-framework-sql2o/src/test/kotlin/com/github/vok/framework/sql2o/MappingTest.kt
+++ vok-framework-sql2o/src/test/kotlin/com/github/vok/framework/sql2o/MappingTest.kt
@@ -5,6 +5,9 @@ import org.junit.AfterClass
 import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
+import java.time.LocalDate
+import java.util.*
+import javax.validation.constraints.NotNull
 import kotlin.test.expect
 
 abstract class AbstractDbTest {
@@ -20,7 +23,7 @@ abstract class AbstractDbTest {
             }
             Sql2oVOKPlugin().init()
             db {
-                con.createQuery("create table if not exists Test ( id bigint primary key auto_increment, name varchar not null, age integer not null )").executeUpdate()
+                con.createQuery("create table if not exists Test ( id bigint primary key auto_increment, name varchar not null, age integer not null, dateOfBirth date, created timestamp )").executeUpdate()
             }
         }
 
@@ -37,7 +40,25 @@ abstract class AbstractDbTest {
 }
 
 @Table("Test")
-data class Person(override var id: Long? = null, var name: String, var age: Int, @Ignore var ignored: String? = null, @Transient var ignored2: Any? = null) : Entity<Long> {
+data class Person(override var id: Long? = null,
+                  var name: String,
+                  var age: Int,
+                  @Ignore var ignored: String? = null,
+                  @Transient var ignored2: Any? = null,
+                  var dateOfBirth: LocalDate? = null,
+
+                  var created: Date? = null
+
+                  ) : Entity<Long> {
+    override fun save() {
+        if (id == null) {
+            // keeping null until https://github.com/aaberg/sql2o/issues/282 is fixed
+//            dateOfBirth = LocalDate.of(1990, 1, 12)
+            created = java.sql.Timestamp(System.currentTimeMillis())
+        }
+        super.save()
+    }
+
     companion object : Dao<Person>
 }
 
