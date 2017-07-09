5ecdf0ef2e4a561e88079d08cde8179531043b28
Martin Vysny <mavi@vaadin.com>
2017-07-09 19:38:04 +0300

    CRUD-sql2o now shows JVM version as well

--- vok-example-crud-sql2o/src/main/kotlin/com/github/vok/example/crud/WelcomeView.kt
+++ vok-example-crud-sql2o/src/main/kotlin/com/github/vok/example/crud/WelcomeView.kt
@@ -34,10 +34,12 @@ To learn more, please visit <a href="http://www.vaadinonkotlin.eu/">Vaadin-on-Ko
         }
         label {
             w = fillParent
-            html("<strong>Vaadin version:</strong> ${Version.getFullVersion()}<br/><strong>Kotlin version:</strong> ${KotlinVersion.CURRENT}")
+            html("<strong>Vaadin version:</strong> ${Version.getFullVersion()}<br/><strong>Kotlin version:</strong> ${KotlinVersion.CURRENT}<br/><strong>JVM version:</strong> $jvmVersion")
         }
     }
 
     override fun enter(event: ViewChangeListener.ViewChangeEvent?) {
     }
 }
+
+val jvmVersion: String get() = System.getProperty("java.version")
