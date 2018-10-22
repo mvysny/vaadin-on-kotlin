2067a934c934d355b9518c5debb55cf9078c5ac4
Martin Vysny <martin@vysny.me>
2018-10-22 09:06:31 +0300

    Initial switch to Kotlin Gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "vaadin-on-kotlin"
+include("vok-framework", "vok-example-crud-jpa", "vok-example-crud7", "vok-framework-jpa", "vok-framework-jpa-compat7", "vok-framework-sql2o", "vok-example-crud-sql2o", "vok-db", "vok-util-vaadin8", "vok-util-vaadin10", "vok-framework-v10-sql2o", "vok-rest", "vok-example-crud-flow", "vok-security")
+
