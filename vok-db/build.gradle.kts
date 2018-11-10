4a41e9d5ba1d183e5ac45ff48eacff90314d77da
Martin Vysny <martin@vysny.me>
2018-11-10 10:09:12 +0200

    v bump

--- vok-db/build.gradle.kts
+++ vok-db/build.gradle.kts
@@ -1,8 +1,7 @@
 dependencies {
     compile(project(":vok-framework"))
-    compile("com.github.vokorm:vok-orm:0.12")
+    compile("com.github.vokorm:vok-orm:0.13")
 }
 
 val configureBintray = ext["configureBintray"] as (artifactId: String, description: String) -> Unit
 configureBintray("vok-db", "VoK: A very simple persistence framework built on top of vok-orm/Sql2o")
-
