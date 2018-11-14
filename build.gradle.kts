7561e1053167645c010f709f82bd66108e8a086d
Martin Vysny <martin@vysny.me>
2018-11-14 19:23:02 +0200

    0.6.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.6.1"
+    version = "0.6.2-SNAPSHOT"
 
     repositories {
         mavenCentral()
