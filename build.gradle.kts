746135cabbe970309610ab85d39ebfc8caad4390
Martin Vysny <martin@vysny.me>
2019-03-06 13:06:40 +0200

    0.7.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.7.1"
+    version = "0.7.2-SNAPSHOT"
 
     repositories {
         mavenCentral()
