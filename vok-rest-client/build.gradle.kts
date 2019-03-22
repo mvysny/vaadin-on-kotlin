ba6569097073b73b913dbe67af6269808b799e10
Martin Vysny <martin@vysny.me>
2019-03-22 12:27:15 +0200

    v bump

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     compile(project(":vok-framework"))
 
     compile("com.google.code.gson:gson:2.8.5")
-    compile("com.squareup.okhttp3:okhttp:3.13.1")
+    compile("com.squareup.okhttp3:okhttp:3.14.0")
     compile("com.github.mvysny.vokdataloader:vok-dataloader:${properties["vok_dataloader_version"]}")
 
     // this project is actually tested in the vok-rest project, where it is used as a test dependency. For tests please see vok-rest
