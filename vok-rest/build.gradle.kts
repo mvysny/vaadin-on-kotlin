2495e4c7cf1525234031a55e3adb70bc2b61273c
Martin Vysny <martin@vysny.me>
2019-08-30 15:09:22 +0300

    v bump

--- vok-rest/build.gradle.kts
+++ vok-rest/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     // testing of the CRUD interface
     testCompile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
     testCompile(project(":vok-rest-client"))
-    testCompile("org.eclipse.jetty.websocket:websocket-server:9.4.12.v20180830")
+    testCompile("org.eclipse.jetty.websocket:websocket-server:9.4.20.v20190813")
     testCompile("org.flywaydb:flyway-core:${properties["flyway_version"]}")
     testCompile("com.h2database:h2:${properties["h2_version"]}")
     testCompile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
