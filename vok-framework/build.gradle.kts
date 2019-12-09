1b966122eff5608f5e1dac5f1c9de142aeba59f0
Martin Vysny <mavi@vaadin.com>
2019-12-09 13:33:44 +0200

    Removed accidental dependency to slf4j-simple from vok-framework

--- vok-framework/build.gradle.kts
+++ vok-framework/build.gradle.kts
@@ -2,9 +2,8 @@ dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compile("org.slf4j:slf4j-api:${properties["slf4j_version"]}")
     testCompile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
-    compile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    testCompile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
 }
 
 val configureBintray = ext["configureBintray"] as (artifactId: String, description: String) -> Unit
 configureBintray("vok-framework", "VoK: The Core classes")
-
