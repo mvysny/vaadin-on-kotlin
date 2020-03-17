0fb6304cc37549d036f10a3c83205cf52ce11070
Martin Vysny <mavi@vaadin.com>
2020-03-17 13:18:38 +0200

    Add FilterBarTest

--- vok-util-vaadin10/src/main/kotlin/eu/vaadinonkotlin/vaadin10/FilterBar.kt
+++ vok-util-vaadin10/src/main/kotlin/eu/vaadinonkotlin/vaadin10/FilterBar.kt
@@ -1,7 +1,9 @@
 package eu.vaadinonkotlin.vaadin10
 
 import com.github.mvysny.karibudsl.v10.DateInterval
+import com.github.mvysny.karibudsl.v10.getColumnBy
 import com.github.mvysny.vokdataloader.Filter
+import com.github.mvysny.vokdataloader.FullTextFilter
 import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.HasSize
 import com.vaadin.flow.component.HasValue
@@ -21,13 +23,15 @@ import kotlin.reflect.KProperty1
 fun <T : Any, F : Any> HeaderRow.asFilterBar(grid: Grid<T>, filterFactory: FilterFactory<F>): FilterBar<T, F> =
         FilterBar(this, grid, filterFactory)
 
+typealias VokFilterBar<T> = FilterBar<T, Filter<T>>
+
 /**
  * Creates a [FilterBar] which produces VOK [Filter]s. Perfect for using with the
  * [VokDataProvider].
  * @param T the bean type present in the [grid]
  * @param grid the owner grid
  */
-fun <T : Any> HeaderRow.asFilterBar(grid: Grid<T>): FilterBar<T, Filter<T>> =
+fun <T : Any> HeaderRow.asFilterBar(grid: Grid<T>): VokFilterBar<T> =
         FilterBar<T, Filter<T>>(this, grid, DataLoaderFilterFactory())
 
 /**
@@ -96,6 +100,28 @@ open class FilterBar<BEAN : Any, FILTER : Any>(
 
     class Binding<BEAN : Any, F : Any>(internal val builder: Builder<BEAN, F, F>) : Serializable {
         /**
+         * Returns the current filter from the filter component.
+         */
+        fun getFilter(): F? = builder.valueGetter()
+
+        /**
+         * Registers a [filterChangeListener], fired when the filter is changed.
+         * @return the listener registration; use [Registration.remove] to remove the listener.
+         */
+        fun addFilterChangeListener(filterChangeListener: () -> Unit) : Registration =
+            (filterComponent as HasValue<*, *>).addValueChangeListener { filterChangeListener() }
+
+        fun unbind() {
+            builder.filterBar.unregisterBinding(this)
+        }
+
+        val filterComponent: Component get() = builder.filterComponent
+
+        fun clearFilter() {
+            (filterComponent as HasValue<*, *>).clear()
+        }
+
+        /**
          * Gradually builds the filter binding.
          * @param BEAN the type of bean present in the Grid
          * @param VALUE this binding is able to extract a value of this type out of the [filterComponent].
@@ -115,35 +141,24 @@ open class FilterBar<BEAN : Any, FILTER : Any>(
             /**
              * Adds a converter to the chain which converts the value from [filterComponent]
              * (or rather from the previous [valueGetter]).
-             * @param closure never receives null: nulls are automatically short-circuited and converted to nulls of type [NEWVALUE].
+             * @param closure never receives nulls nor blank strings: nulls are automatically short-circuited and converted to nulls of type [NEWVALUE].
              * @param NEWVALUE the new value produced by the new builder (which is now the head of the conversion chain).
              * @return the new converter chain head
              */
             fun <NEWVALUE : Any> withConverter(closure: (VALUE) -> NEWVALUE?): Builder<BEAN, NEWVALUE, TARGETFILTER> {
                 val chainedValueGetter: () -> NEWVALUE? = {
                     val prevValue: VALUE? = valueGetter()
-                    if (prevValue == null) null else closure(prevValue)
+                    val isBlankString: Boolean = (prevValue as? String)?.isBlank() ?: false
+                    if (prevValue == null || isBlankString) null else closure(prevValue)
                 }
                 return Builder(filterFactory, filterBar, column, filterComponent, chainedValueGetter)
             }
 
             fun eq(): Binding<BEAN, TARGETFILTER> = withConverter { filterFactory.eq(propertyName, it) } .bind()
-        }
 
-        /**
-         * Returns the current filter from the filter component.
-         */
-        fun getFilter(): F? = builder.valueGetter()
+            fun le(): Binding<BEAN, TARGETFILTER> = withConverter { filterFactory.le(propertyName, it) } .bind()
 
-        /**
-         * Registers a [filterChangeListener], fired when the filter is changed.
-         * @return the listener registration; use [Registration.remove] to remove the listener.
-         */
-        fun addFilterChangeListener(filterChangeListener: () -> Unit) : Registration =
-            (builder.filterComponent as HasValue<*, *>).addValueChangeListener { filterChangeListener() }
-
-        fun unbind() {
-            builder.filterBar.unregisterBinding(this)
+            fun ge(): Binding<BEAN, TARGETFILTER> = withConverter { filterFactory.ge(propertyName, it) } .bind()
         }
     }
 
@@ -162,9 +177,21 @@ open class FilterBar<BEAN : Any, FILTER : Any>(
     }
 
     /**
-     * Removes all filter components from this filter bar.
+     * Clears all filter components.
      */
     fun clear() {
+        bindings.keys.forEach { it.clearFilter() }
+    }
+
+    /**
+     * Returns all current bindings.
+     */
+    fun getBindings(): List<Binding<BEAN, FILTER>> = bindings.keys.toList()
+
+    /**
+     * Removes all filter components from this filter bar.
+     */
+    fun removeAllBindings() {
         bindings.keys.toList().forEach { it.unbind() }
     }
 
@@ -173,6 +200,9 @@ open class FilterBar<BEAN : Any, FILTER : Any>(
         checkNotNull(binding) { "No binding for column ${column.key}: $column" }
         return binding
     }
+
+    fun getBindingFor(property: KProperty1<BEAN, *>): Binding<BEAN, FILTER> =
+            getBindingFor(grid.getColumnBy(property))
 
     /**
      * Configures every Vaadin UI filter [field]. By default the width is set to 100%
@@ -184,6 +214,10 @@ open class FilterBar<BEAN : Any, FILTER : Any>(
         (field as? ComboBox<*>)?.isClearButtonVisible = true
         (field as? HasValueChangeMode)?.valueChangeMode = ValueChangeMode.LAZY
     }
+
+    fun getFilterComponent(column: Grid.Column<BEAN>) = getBindingFor(column).filterComponent
+
+    fun getFilterComponent(property: KProperty1<BEAN, *>) = getFilterComponent(grid.getColumnBy(property))
 }
 
 fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, FILTER, FILTER>.bind(): FilterBar.Binding<BEAN, FILTER> {
@@ -193,7 +227,7 @@ fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, FILTER, FILTER>.bi
 }
 
 fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, String, FILTER>.ilike(): FilterBar.Binding<BEAN, FILTER> =
-        withConverter { if (it.isNullOrBlank()) null else filterFactory.ilike(propertyName, it) }
+        withConverter { if (it.isBlank()) null else filterFactory.ilike(propertyName, it) }
                 .bind()
 
 @JvmName("numberIntervalInRange")
@@ -203,8 +237,16 @@ fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, NumberInterval<Dou
 
 @JvmName("dateIntervalInRange")
 fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, DateInterval, FILTER>.inRange(fieldType: KClass<*>): FilterBar.Binding<BEAN, FILTER> =
-        withConverter { it.toFilter(propertyName, filterFactory, fieldType.java) }
+        inRange(fieldType.java)
+
+@JvmName("dateIntervalInRange2")
+fun <BEAN : Any, FILTER: Any> FilterBar.Binding.Builder<BEAN, DateInterval, FILTER>.inRange(fieldType: Class<*>): FilterBar.Binding<BEAN, FILTER> =
+        withConverter { it.toFilter(propertyName, filterFactory, fieldType) }
                 .bind()
 
 inline fun <BEAN : Any, FILTER: Any, reified V> FilterBar.Binding.Builder<BEAN, DateInterval, FILTER>.inRange(property: KProperty1<BEAN, V>): FilterBar.Binding<BEAN, FILTER> =
         inRange(V::class)
+
+fun <BEAN : Any> FilterBar.Binding.Builder<BEAN, String, Filter<BEAN>>.fullText(): FilterBar.Binding<BEAN, Filter<BEAN>> =
+        withConverter<Filter<BEAN>> { if (it.isBlank()) null else FullTextFilter<BEAN>(propertyName, it) }
+                .bind()
--- vok-util-vaadin10/src/test/kotlin/eu/vaadinonkotlin/vaadin10/FilterBarTest.kt
+++ vok-util-vaadin10/src/test/kotlin/eu/vaadinonkotlin/vaadin10/FilterBarTest.kt
@@ -4,7 +4,10 @@ import com.github.mvysny.kaributesting.v10.*
 import com.github.mvysny.dynatest.DynaTest
 import com.github.mvysny.dynatest.expectList
 import com.github.mvysny.karibudsl.v10.DateRangePopup
+import com.github.mvysny.karibudsl.v10.component
 import com.github.mvysny.karibudsl.v10.getAll
+import com.github.mvysny.karibudsl.v10.getColumnBy
+import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.data.provider.ListDataProvider
@@ -12,29 +15,36 @@ import java.time.LocalDate
 import java.util.*
 import kotlin.test.expect
 
-class FilterRowTest : DynaTest({
+fun FilterBar<*, *>.getFilterComponents(): List<Component> = grid.columns.mapNotNull { headerRow.getCell(it).component }
+
+class FilterBarTest : DynaTest({
     beforeEach { MockVaadin.setup() }
     afterEach { MockVaadin.tearDown() }
 
     test("Test simple auto-generated filters") {
         data class Person(var name: String, var age: Int, val dob: Date, val dateOfMarriage: LocalDate)
-        val grid = Grid<Person>(Person::class.java)
-        val filterRow = grid.appendHeaderRow().generateFilterComponents2(grid, Person::class, PredicateFilterFactory<Person>())
-        expect<Class<*>>(TextField::class.java) { filterRow.getFilterComponent(Person::name).javaClass }
-        expect<Class<*>>(NumberRangePopup::class.java) { filterRow.getFilterComponent(Person::age).javaClass }
-        expect<Class<*>>(DateRangePopup::class.java) { filterRow.getFilterComponent(Person::dob).javaClass }
-        expect<Class<*>>(DateRangePopup::class.java) { filterRow.getFilterComponent(Person::dateOfMarriage).javaClass }
-        filterRow.serializeToBytes()
+        val grid: Grid<Person> = Grid(Person::class.java)
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid, PredicateFilterFactory<Person>())
+        filterBar.forField(TextField(), grid.getColumnBy(Person::name)).ilike()
+        filterBar.forField(NumberRangePopup(), grid.getColumnBy(Person::age)).inRange()
+        filterBar.forField(DateRangePopup(), grid.getColumnBy(Person::dob)).inRange(Person::dob)
+        filterBar.forField(DateRangePopup(), grid.getColumnBy(Person::dateOfMarriage)).inRange(Person::dateOfMarriage)
+        expect<Class<*>>(TextField::class.java) { filterBar.getFilterComponent(Person::name).javaClass }
+        expect<Class<*>>(NumberRangePopup::class.java) { filterBar.getFilterComponent(Person::age).javaClass }
+        expect<Class<*>>(DateRangePopup::class.java) { filterBar.getFilterComponent(Person::dob).javaClass }
+        expect<Class<*>>(DateRangePopup::class.java) { filterBar.getFilterComponent(Person::dateOfMarriage).javaClass }
+        filterBar.serializeToBytes()
     }
 
     test("string filter") {
         data class Person(var name: String)
         val grid = Grid<Person>(Person::class.java)
-        val filterRow = grid.appendHeaderRow().generateFilterComponents2(grid, Person::class, PredicateFilterFactory<Person>())
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid, PredicateFilterFactory<Person>())
+        filterBar.forField(TextField(), grid.getColumnBy(Person::name)).ilike()
         grid.setItems(Person("foo"))
         expect(1) { grid.dataProvider._size() }
 
-        val nameFilter = filterRow.getFilterComponent(Person::name)
+        val nameFilter = filterBar.getFilterComponent(Person::name) as TextField
         nameFilter._value = "bar"
         expect(0) { grid.dataProvider._size() }
         nameFilter._value = "foo"
@@ -44,25 +54,30 @@ class FilterRowTest : DynaTest({
     test("filter components from cleared filter bar won't affect the grid anymore") {
         data class Person(var name: String)
         val grid = Grid<Person>(Person::class.java)
-        val filterRow = grid.appendHeaderRow().generateFilterComponents2(grid, Person::class, PredicateFilterFactory<Person>())
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid, PredicateFilterFactory<Person>())
+        filterBar.forField(TextField(), grid.getColumnBy(Person::name)).ilike()
         grid.setItems(Person("foo"))
-        val nameFilter = filterRow.getFilterComponent(Person::name)
+        val nameFilter = filterBar.getFilterComponent(Person::name) as TextField
 
         expect(1) { grid.dataProvider._size() }
         nameFilter._value = "bar"
         expect(0) { grid.dataProvider._size() }
-        filterRow.clear()
+        filterBar.clear()
+        expect("") { nameFilter._value }
         expect(1) { grid.dataProvider._size() }
         nameFilter._value = "baz"
+        expect(0) { grid.dataProvider._size() }
+        nameFilter._value = "foo"
         expect(1) { grid.dataProvider._size() }
     }
 
     test("filter components from cleared filter bar gone") {
         data class Person(var name: String)
         val grid = Grid<Person>(Person::class.java)
-        val filterRow = grid.appendHeaderRow().generateFilterComponents2(grid, Person::class, PredicateFilterFactory<Person>())
-        filterRow.clear()
-        expect(mapOf()) { filterRow.getFilterComponents() }
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid, PredicateFilterFactory<Person>())
+        filterBar.forField(TextField(), grid.getColumnBy(Person::name)).ilike()
+        filterBar.removeAllBindings()
+        expectList() { filterBar.getFilterComponents() }
     }
 
     // test for https://www.github.com/mvysny/vaadin-on-kotlin/issues/17
@@ -75,7 +90,8 @@ class FilterRowTest : DynaTest({
         }
         expectList() { grid.dataProvider!!.getAll() }
 
-        val filterBar = filterRow.generateFilterComponents2(grid, Person::class, PredicateFilterFactory<Person>())
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid, PredicateFilterFactory<Person>())
+        filterBar.forField(TextField(), grid.getColumnBy(Person::name)).ilike()
 
         // now let's create and set another data provider. If the generateFilterComponents grabs the DP eagerly, it will ignore this second DP.
         grid.dataProvider = ListDataProvider<Person>(listOf(Person("foobar")))
