d4b29354b0a609bb05efb77c152aee3f5ab1278c
Martin Vysny <mavi@vaadin.com>
2020-03-17 13:25:46 +0200

    Fixed tests

--- vok-framework-v10-vokdb/src/test/kotlin/eu/vaadinonkotlin/vaadin10/vokdb/VaadinFiltersTest.kt
+++ vok-framework-v10-vokdb/src/test/kotlin/eu/vaadinonkotlin/vaadin10/vokdb/VaadinFiltersTest.kt
@@ -5,11 +5,11 @@ import com.github.mvysny.dynatest.expectList
 import com.github.mvysny.kaributesting.v10.MockVaadin
 import com.github.mvysny.karibudsl.v10.addColumnFor
 import com.github.mvysny.karibudsl.v10.getAll
-import com.vaadin.flow.component.Component
+import com.github.mvysny.karibudsl.v10.getColumnBy
+import com.github.mvysny.kaributesting.v10._value
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.textfield.TextField
-import eu.vaadinonkotlin.vaadin10.generateFilterComponents
-import eu.vaadinonkotlin.vaadin10.withFilter
+import eu.vaadinonkotlin.vaadin10.*
 
 class VaadinFiltersTest : DynaTest({
     beforeEach { MockVaadin.setup() }
@@ -25,14 +25,14 @@ class VaadinFiltersTest : DynaTest({
 
         grid.dataProvider = Person.dataProvider.withFilter { Person::personName eq "foo" }
         expectList() { grid.dataProvider!!.getAll() }
-        val filterComponents: Map<String, Component> =
-            grid.appendHeaderRow().generateFilterComponents(grid, Person::class).getFilterComponents()
+        val filterBar = grid.appendHeaderRow().asFilterBar(grid)
+        filterBar.forField(TextField(), grid.getColumnBy(Person::personName)).ilike()
 
         // now let's create another data provider
         grid.dataProvider = Person.dataProvider
 
         // if the generateFilterComponents function reflects the DP change, it will overwrite the filter, making the DP match the person
-        (filterComponents["personName"] as TextField).value = "foobar"
+        (filterBar.getFilterComponent(Person::personName) as TextField)._value = "foobar"
         expectList("foobar") { grid.dataProvider!!.getAll().map { it.personName } }
     }
 })
