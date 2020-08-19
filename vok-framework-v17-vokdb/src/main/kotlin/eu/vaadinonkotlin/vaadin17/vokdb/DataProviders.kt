4a94126c05d5b3bb42271f27f0daf011b39bf462
Martin Vysny <mavi@vaadin.com>
2020-08-19 19:21:34 +0300

    Implemented HasDataView.setDataLoader(DataLoader, idResolver). Closes #51

--- vok-framework-v17-vokdb/src/main/kotlin/eu/vaadinonkotlin/vaadin17/vokdb/DataProviders.kt
+++ vok-framework-v17-vokdb/src/main/kotlin/eu/vaadinonkotlin/vaadin17/vokdb/DataProviders.kt
@@ -6,6 +6,8 @@ import eu.vaadinonkotlin.vaadin10.DataLoaderAdapter
 import eu.vaadinonkotlin.vaadin10.vokdb.asDataProvider
 import com.vaadin.flow.data.provider.HasDataView
 import com.vaadin.flow.data.provider.DataView
+import eu.vaadinonkotlin.vaadin10.asDataProvider
+
 /**
  * Sets given data loader to this Grid, by the means of wrapping the data loader
  * via [DataLoaderAdapter] and setting it
@@ -13,4 +15,16 @@ import com.vaadin.flow.data.provider.DataView
  */
 public fun <T: KEntity<*>, V: DataView<T>> HasDataView<T, V>.setDataLoader(dataLoader: DataLoader<T>) {
     setItems(dataLoader.asDataProvider())
+}
+
+/**
+ * Sets given [dataLoader] to this [Grid], by the means of wrapping the [dataLoader] via [DataLoaderAdapter] and setting it
+ * as a (configurable) [Grid.getDataProvider].
+ * @param idResolver provides unique ID for every item. The ID is then used to differentiate items.
+ * See [com.vaadin.flow.data.provider.DataProvider.getId] for more details. Typically every item
+ * has a primary key of type [Long], but any Java/Kotlin object with properly written [Any.equals] and [Any.hashCode] can act as the ID,
+ * including the item itself.
+ */
+public fun <T: Any, V: DataView<T>> HasDataView<T, V>.setDataLoader(dataLoader: DataLoader<T>, idResolver: (T)->Any) {
+    setItems(dataLoader.asDataProvider(idResolver))
 }
