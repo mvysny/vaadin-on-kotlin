03a76f00754f490ee1fe98791a8a1210ddc20ca1
Martin Vysny <mavi@vaadin.com>
2021-02-05 09:08:05 +0200

    publish directly to maven central since bintray is terminated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,12 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.util.*
 
 plugins {
     kotlin("jvm") version "1.4.30"
     id("org.gretty") version "3.0.3"
-    id("com.jfrog.bintray") version "1.8.3"
     `maven-publish`
-    id("org.jetbrains.dokka") version "1.4.0"
     id("com.vaadin") version "0.14.3.7" apply(false)
+    signing
 }
 
 defaultTasks("clean", "build")
@@ -20,7 +16,8 @@ allprojects {
     version = "0.9.2-SNAPSHOT"
 
     repositories {
-        jcenter() // dokka is not in mavenCentral()
+        mavenCentral()
+        jcenter() // Gretty is not in mavenCentral()
         maven { setUrl("https://maven.vaadin.com/vaadin-addons") }  // because of JPA Container
         maven { setUrl("https://maven.vaadin.com/vaadin-prereleases/") }
     }
@@ -37,8 +34,7 @@ subprojects {
     apply {
         plugin("maven-publish")
         plugin("kotlin")
-        plugin("com.jfrog.bintray")
-        plugin("org.jetbrains.dokka")
+        plugin("org.gradle.signing")
     }
 
     tasks.withType<KotlinCompile> {
@@ -61,26 +57,25 @@ subprojects {
     // creates a reusable function which configures proper deployment to Bintray
     ext["configureBintray"] = { artifactId: String, description: String ->
 
-        val local = Properties()
-        val localProperties: File = rootProject.file("local.properties")
-        if (localProperties.exists()) {
-            localProperties.inputStream().use { local.load(it) }
+        java {
+            withJavadocJar()
+            withSourcesJar()
         }
 
-        val java: JavaPluginConvention = convention.getPluginByName("java")
-
-        val sourceJar = task("sourceJar", Jar::class) {
-            dependsOn(tasks.findByName("classes"))
-            classifier = "sources"
-            from(java.sourceSets["main"].allSource)
-        }
-
-        val javadocJar = task("javadocJar", Jar::class) {
-            from(tasks["dokkaJavadoc"])
-            archiveClassifier.set("javadoc")
+        tasks.withType<Javadoc> {
+            isFailOnError = false
         }
 
         publishing {
+            repositories {
+                maven {
+                    setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                        password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+                    }
+                }
+            }
             publications {
                 create("mavenJava", MavenPublication::class.java).apply {
                     groupId = project.group.toString()
@@ -109,27 +104,12 @@ subprojects {
                         }
                     }
                     from(components.findByName("java")!!)
-                    artifact(sourceJar)
-                    artifact(javadocJar)
                 }
             }
         }
 
-        bintray {
-            user = local.getProperty("bintray.user")
-            key = local.getProperty("bintray.key")
-            pkg(closureOf<BintrayExtension.PackageConfig> {
-                repo = "github"
-                name = "eu.vaadinonkotlin"
-                setLicenses("MIT")
-                vcsUrl = "https://github.com/mvysny/vaadin-on-kotlin"
-                publish = true
-                setPublications("mavenJava")
-                version(closureOf<BintrayExtension.VersionConfig> {
-                    this.name = project.version.toString()
-                    released = Date().toString()
-                })
-            })
+        signing {
+            sign(publishing.publications["mavenJava"])
         }
     }
 }
