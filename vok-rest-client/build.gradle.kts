75809e2fb6d8ef2506d2db0f3d9a9b9072cea49b
Martin Vysny <mavi@vaadin.com>
2021-02-12 10:10:44 +0200

    #60 initial support for Moshi

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     compile(project(":vok-framework"))
 
     compile("com.google.code.gson:gson:2.8.5")
+    compile("com.squareup.moshi:moshi:1.11.0")
     compile("com.squareup.okhttp3:okhttp:4.0.0")
     compile("com.github.mvysny.vokdataloader:vok-dataloader:${properties["vok_dataloader_version"]}")
 
@@ -9,6 +10,9 @@ dependencies {
     testCompile("com.github.mvysny.dynatest:dynatest-engine:${properties["dynatest_version"]}")
     testCompile("io.javalin:javalin:${properties["javalin_version"]}")
     testCompile("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+
+    // temporary: add kotlin-reflect until https://github.com/mvysny/vaadin-on-kotlin/issues/60 is sorted out.
+    testCompile("com.squareup.moshi:moshi-kotlin:1.11.0")
 }
 
 kotlin {
