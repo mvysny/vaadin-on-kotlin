250709b3b5db33a0fb0d1d31443c68d9288fee37
Martin Vysny <martin@vysny.me>
2021-09-08 19:20:50 +0300

    v bump to 0.11.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.10.1-SNAPSHOT"
+    version = "0.11.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
