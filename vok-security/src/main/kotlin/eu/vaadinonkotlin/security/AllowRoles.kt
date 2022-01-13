1efb41c221a77337e66bbaf52d261792bdd95662
Martin Vysny <mavi@vaadin.com>
2022-01-13 11:44:09 +0200

    fix AllowRoles kdoc

--- vok-security/src/main/kotlin/eu/vaadinonkotlin/security/AllowRoles.kt
+++ vok-security/src/main/kotlin/eu/vaadinonkotlin/security/AllowRoles.kt
@@ -30,8 +30,8 @@ public typealias AllowRoles = RolesAllowed
 /**
  * Allows anybody to see this view, even if there is no user logged in.
  *
- * Intended to be attached to Vaadin8 `View` or Vaadin10 `@Route`-annotated component, to restrict which users can see the page.
- * The logic which checks this annotation is present in the `vok-util-vaadin8` or `vok-util-vaadin10` project.
+ * Intended to be attached to Vaadin `@Route`-annotated component, to restrict which users can see the page.
+ * The logic which checks this annotation is present in the `vok-util-vaadin` project.
  *
  * Only one of the [AllowRoles], [AllowAll] and [AllowAllUsers] can be present. If more than one is present, it's a configuration error and a [RuntimeException] will be thrown.
  * If none is present, nobody can see that view. This is to prevent views accidentally exposed by omitting the security annotation on them.
