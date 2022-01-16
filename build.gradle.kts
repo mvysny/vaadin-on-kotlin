712f180ac1ae8e9ed4d4db096850b065171499e3
Martin Vysny <mavi@vaadin.com>
2022-01-16 11:00:10 +0200

    0.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.12.1-SNAPSHOT"
+    version = "0.12.1"
 
     repositories {
         mavenCentral()
