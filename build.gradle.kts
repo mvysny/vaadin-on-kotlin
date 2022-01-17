28c394de93d6152c9d98a41aa50a114724bc1f3a
Martin Vysny <mavi@vaadin.com>
2022-01-17 10:05:26 +0200

    0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.9.2-SNAPSHOT"
+    version = "0.9.2"
 
     repositories {
         jcenter() // dokka is not in mavenCentral()
