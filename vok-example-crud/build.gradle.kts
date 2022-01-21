71f77b175ad5e7c9e90a834f60101406efe2c37e
Martin Vysny <mavi@vaadin.com>
2022-01-21 07:55:47 +0200

    vaadin v bump

--- vok-example-crud/build.gradle.kts
+++ vok-example-crud/build.gradle.kts
@@ -45,7 +45,6 @@ vaadin {
     if (gradle.startParameter.taskNames.contains("stage")) {
         productionMode = true
     }
-    nodeVersion = "v16.13.2" // workaround for https://github.com/vaadin/flow/issues/12732
 }
 
 // Heroku
