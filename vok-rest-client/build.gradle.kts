b7772c98d3d8630aa69429682811c82c3ebf6d06
Martin Vysny <mavi@vaadin.com>
2022-06-15 08:18:28 +0300

    dep v bump

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     api(project(":vok-framework"))
 
     api("com.google.code.gson:gson:${properties["gson_version"]}")
-    api("com.squareup.moshi:moshi:1.12.0")
-    api("com.squareup.okhttp3:okhttp:4.9.1")
+    api("com.squareup.moshi:moshi:1.13.0")
+    api("com.squareup.okhttp3:okhttp:4.9.3")
     api("com.github.mvysny.vokdataloader:vok-dataloader:${properties["vok_dataloader_version"]}")
     // workaround for https://github.com/google/gson/issues/1059
     implementation("com.fatboyindustrial.gson-javatime-serialisers:gson-javatime-serialisers:1.1.1")
@@ -16,7 +16,7 @@ dependencies {
     testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
 
     // temporary: add kotlin-reflect until https://github.com/mvysny/vaadin-on-kotlin/issues/60 is sorted out.
-    testImplementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+    testImplementation("com.squareup.moshi:moshi-kotlin:1.13.0")
     testImplementation(kotlin("reflect")) // make sure to use the same kotlin-reflect version as the kotlin language itself.
 }
 
