624ac94be0bada35d9f5aa4df742db278c446e2b
Martin Vysny <mavi@vaadin.com>
2022-09-05 13:35:47 +0300

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
     id("org.gretty") version "3.0.6"
     `maven-publish`
-    id("com.vaadin") version "23.1.6" apply(false)
+    id("com.vaadin") version "23.2.0" apply(false)
     signing
 }
 
