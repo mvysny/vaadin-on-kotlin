a296c5070523931ea0e4dfefc7797da3bb8d84c0
Martin Vysny <mavi@vaadin.com>
2022-10-30 15:25:23 +0200

    kdoc

--- vok-framework/src/main/kotlin/eu/vaadinonkotlin/Services.kt
+++ vok-framework/src/main/kotlin/eu/vaadinonkotlin/Services.kt
@@ -8,11 +8,11 @@ import kotlin.reflect.KClass
  *
  * Register your stateless services simply as `val Services.yourService: YourService get() = YourService()`
  *
- * Register your singleton services as `val Services.yourService: YourService get() = singletons.getOrCreate(YourService::class) { YourService() }`.
+ * Register your singleton services as `val Services.yourService: YourService get() = singletons.getOrCreate { YourService() }`.
  * The service will be created lazily, and at most once.
  *
  * WARNING: Calling [VaadinOnKotlin.destroy] will clean up the singleton instances. If you need to
- * deinit your services, you have to do it beforehand.
+ * de-init your services, you have to do it beforehand.
  *
  * See [VoK: Services](https://www.vaadinonkotlin.eu/services/) documentation for more details.
  */
@@ -20,14 +20,15 @@ public object Services {
     /**
      * Internal: Only serves for singleton service definition. Don't use to look up your application services.
      *
-     * Register your singleton services as `val Services.yourService: YourService get() = singletons.getOrCreate(YourService::class) { YourService() }`.
+     * Register your singleton services as `val Services.yourService: YourService get() = singletons.getOrCreate { YourService() }`.
      * The service will be created lazily, and at most once.
      */
     public val singletons: Singletons = Singletons()
 }
 
 /**
- * A registry of singleton services.
+ * A registry of JVM singleton services (= there's at most 1 instance of the service
+ * class in the JVM).
  */
 public class Singletons {
     private val instances = ConcurrentHashMap<Class<*>, Any>()
@@ -41,15 +42,42 @@ public class Singletons {
         check(instances.putIfAbsent(serviceClass.java, instance) == null) { "Service $serviceClass is already registered" }
     }
 
+    /**
+     * Looks up a service by its [serviceClass] in the list of JVM singletons. If it's missing,
+     * the [instanceInitializer] will be called, to create the instance of the service.
+     *
+     * The [instanceInitializer] will be called at most once; while the initializer is running,
+     * any further lookups of the same service will block until the initializer is ready.
+     *
+     * Fails if [VaadinOnKotlin.isStarted] is false. That means that in your bootstrap, first
+     * call [VaadinOnKotlin.init], and only then start initializing your singleton services.
+     * During shutdown, first de-init all of your singleton services, and only then call
+     * [VaadinOnKotlin.destroy].
+     */
     @Suppress("UNCHECKED_CAST")
     public fun <T: Any> getOrCreate(serviceClass: KClass<T>, instanceInitializer: () -> T): T {
         check(VaadinOnKotlin.isStarted) { "VoK is not started" }
         return instances.computeIfAbsent(serviceClass.java) { instanceInitializer() } as T
     }
 
+    /**
+     * Looks up a service by its class in the list of JVM singletons. If it's missing,
+     * the [instanceInitializer] will be called, to create the instance of the service.
+     *
+     * The [instanceInitializer] will be called at most once; while the initializer is running,
+     * any further lookups of the same service will block until the initializer is ready.
+     *
+     * Fails if [VaadinOnKotlin.isStarted] is false. That means that in your bootstrap, first
+     * call [VaadinOnKotlin.init], and only then start initializing your singleton services.
+     * During shutdown, first de-init all of your singleton services, and only then call
+     * [VaadinOnKotlin.destroy].
+     */
     public inline fun <reified T: Any> getOrCreate(noinline instanceInitializer: () -> T): T =
         getOrCreate(T::class, instanceInitializer)
 
+    /**
+     * Removes all singleton instances; further service lookups will create a new instance.
+     */
     internal fun destroy() {
         instances.clear()
     }
