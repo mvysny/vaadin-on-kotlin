bd476468672624dabe303232ed321ac16159cfee
Martin Vysny <mavi@vaadin.com>
2023-01-19 08:58:35 +0200

    Removed obsolete VokAccessAnnotationChecker

--- vok-security/src/main/kotlin/eu/vaadinonkotlin/security/VokViewAccessChecker.kt
+++ vok-security/src/main/kotlin/eu/vaadinonkotlin/security/VokViewAccessChecker.kt
@@ -9,21 +9,6 @@ import java.util.function.Function
 import javax.servlet.http.HttpServletRequest
 
 /**
- * Completely bypasses the Servlet Container security. Instead of checking the outcome of
- * [HttpServletRequest.getUserPrincipal] and [HttpServletRequest.isUserInRole],
- * it checks the [VaadinOnKotlin.loggedInUserResolver] instead.
- */
-public object VokAccessAnnotationChecker : AccessAnnotationChecker() {
-    override fun hasAccess(
-        cls: Class<*>,
-        request: HttpServletRequest
-    ): Boolean {
-        val userResolver: LoggedInUserResolver = VaadinOnKotlin.loggedInUserResolver
-        return hasAccess(cls, userResolver.getCurrentUser()) { role -> userResolver.hasRole(role) }
-    }
-}
-
-/**
  * Checks that a user is logged in. Uses standard Vaadin [ViewAccessChecker] but
  * obtains the user from [VaadinOnKotlin.loggedInUserResolver] rather than from
  * [HttpServletRequest.getUserPrincipal] and [HttpServletRequest.isUserInRole].
@@ -35,7 +20,7 @@ public object VokAccessAnnotationChecker : AccessAnnotationChecker() {
  * See [vok-security README](https://github.com/mvysny/vaadin-on-kotlin/blob/master/vok-security/README.md)
  * on how to use this class properly.
  */
-public class VokViewAccessChecker : ViewAccessChecker(VokAccessAnnotationChecker) {
+public class VokViewAccessChecker : ViewAccessChecker(AccessAnnotationChecker()) {
     init {
         enable()
     }
