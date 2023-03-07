76562f7e134b05d34dcbd85ebecfecc340d01aa6
Martin Vysny <mavi@vaadin.com>
2023-03-07 23:41:49 +0200

    0.15.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.14.2-SNAPSHOT"
+    version = "0.15.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
