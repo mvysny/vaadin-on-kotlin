4cd6466c0a8d9349da40765f7b562a39ca1286b7
Martin Vysny <mavi@vaadin.com>
2023-04-26 20:42:28 +0300

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.21"
-    id("com.vaadin") version "24.0.0" apply(false)
+    id("com.vaadin") version "24.0.5" apply(false)
     `maven-publish`
     signing
 }
