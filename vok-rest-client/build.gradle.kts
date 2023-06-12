c1441923db061b0825f80ed9338e9def941c6be7
Martin Vysny <mavi@vaadin.com>
2023-06-12 20:51:09 +0300

    dep v bump

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
 
     api("com.google.code.gson:gson:${properties["gson_version"]}")
     // can't use java.net.http.HttpClient since it lacks URIBuilder
-    api("com.squareup.okhttp3:okhttp:4.9.3")
+    api("com.squareup.okhttp3:okhttp:4.10.0")
     api("com.github.mvysny.vokdataloader:vok-dataloader:${properties["vok_dataloader_version"]}")
     // workaround for https://github.com/google/gson/issues/1059
     implementation("com.fatboyindustrial.gson-javatime-serialisers:gson-javatime-serialisers:1.1.1")
