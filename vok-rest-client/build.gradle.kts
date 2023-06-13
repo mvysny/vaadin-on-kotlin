c53e9b7536d91d1bd5e883cc418b188db61408cb
Martin Vysny <mavi@vaadin.com>
2023-06-13 06:58:25 +0300

    minor

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -2,8 +2,9 @@ dependencies {
     api(project(":vok-framework"))
 
     api("com.google.code.gson:gson:${properties["gson_version"]}")
-    // can't use java.net.http.HttpClient since it lacks URIBuilder; uribuilder-tiny lacks
-    // support for duplicite query parameters: https://github.com/moznion/uribuilder-tiny/issues/7
+    // can't use java.net.http.HttpClient since it lacks URIBuilder
+    // 1. uribuilder-tiny lacks support for duplicite query parameters: https://github.com/moznion/uribuilder-tiny/issues/7
+    // 2. https://github.com/httpcache4j/uribuilder is immutable which is just plain dumb
     api("com.squareup.okhttp3:okhttp:4.10.0")
     api("com.github.mvysny.vokdataloader:vok-dataloader:${properties["vok_dataloader_version"]}")
     // workaround for https://github.com/google/gson/issues/1059
