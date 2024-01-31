b9a6611e35506bb77fd471f72a6389499a119781
Martin Vysny <mavi@vaadin.com>
2024-01-31 16:21:00 +0200

    Gradle: take version catalog into use

--- vok-rest-client/build.gradle.kts
+++ vok-rest-client/build.gradle.kts
@@ -1,22 +1,19 @@
 dependencies {
     api(project(":vok-framework"))
-    api("com.gitlab.mvysny.jdbiorm:jdbi-orm:${properties["jdbi_orm_version"]}") // only used for the Condition API
+    api(libs.jdbiorm) // only used for the Condition API
 
-    api("com.google.code.gson:gson:${properties["gson_version"]}")
-    api("com.gitlab.mvysny.apache-uribuilder:uribuilder:5.2.1")
-    // workaround for https://github.com/google/gson/issues/1059
-    implementation("com.fatboyindustrial.gson-javatime-serialisers:gson-javatime-serialisers:1.1.1")
+    api(libs.bundles.gson)
+    api(libs.apache.uribuilder)
 
     // this project is actually tested in the vok-rest project, where it is used as a test dependency. For tests please see vok-rest
-    testImplementation("com.github.mvysny.dynatest:dynatest:${properties["dynatest_version"]}")
+    testImplementation(libs.dynatest)
     testImplementation("io.javalin:javalin:${properties["javalin_version"]}") {
         exclude(group = "org.eclipse.jetty")
         exclude(group = "org.eclipse.jetty.websocket")
         exclude(group = "com.fasterxml.jackson.core")
     }
-    testImplementation("org.eclipse.jetty.ee10:jetty-ee10-webapp:${properties["jetty_version"]}")
-    testImplementation("org.eclipse.jetty.ee10.websocket:jetty-ee10-websocket-jakarta-server:${properties["jetty_version"]}")
-    testImplementation("org.slf4j:slf4j-simple:${properties["slf4j_version"]}")
+    testImplementation(libs.bundles.jetty)
+    testImplementation(libs.slf4j.simple)
 }
 
 kotlin {
