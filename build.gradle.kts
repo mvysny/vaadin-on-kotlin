7b8a1a89b4379be2971c7d7bde38440b4a5dcdac
Martin Vysny <mavi@vaadin.com>
2024-08-08 08:24:19 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.10"
     `maven-publish`
     signing
 }
