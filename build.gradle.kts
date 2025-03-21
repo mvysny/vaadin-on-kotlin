1df704ef727d9be807849da1870c5807b3d15470
Martin Vysny <mavi@vaadin.com>
2025-03-21 08:59:46 +0200

    kotlin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
     `maven-publish`
     signing
 }
