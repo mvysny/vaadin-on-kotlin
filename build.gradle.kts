8e91508e5b749b55c950abd835a456873b42b32e
Martin Vysny <martin@vysny.me>
2026-05-12 20:48:49 +0300

    0.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ defaultTasks("clean", "build")
 
 allprojects {
     group = "eu.vaadinonkotlin"
-    version = "0.19.0-SNAPSHOT"
+    version = "0.19.0"
 
     repositories {
         mavenCentral()
